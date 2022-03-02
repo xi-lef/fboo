57283d4fdc49be97afd23a8d1bf7886f00077d0b
Felix Windsheimer <felix.windsheimer@fau.de>
2022-03-02 15:35:57 +0100

    util.hpp: improve template requirements

--- include/fboo/util.hpp
+++ include/fboo/util.hpp
@@ -23,42 +23,33 @@ inline constexpr bool is_specialization = false;
 template <template <class...> class T, class... Args>
 inline constexpr bool is_specialization<T<Args...>, T> = true;
 
-template <class T>
-concept is_vector = is_specialization<T, std::vector>;
-
-template <is_vector It>
+template <class It>
+requires is_specialization<It, std::vector>
 std::ostream &operator<<(std::ostream &os, const It &it) {
-    if (it.empty()) {
-        return os;
-    }
-
-    os << it.front();
-    for (const auto &i : it | std::views::drop(1)) {
-        os << ", " << i;
+    if (!it.empty()) {
+        os << it.front();
+        for (const auto &i : it | std::views::drop(1)) {
+            os << ", " << i;
+        }
     }
     return os;
 }
 
 template <class Subclass, std::ranges::range It>
-requires requires(It it) {
-    { it[0].get() } -> std::convertible_to<void *>;
-    // TODO is unique_ptr?
-    //{ it.at(0) } -> std::same_as<std::unique_ptr<decltype(it.at(0).get())>>;
-    //{ it.at(0) } -> std::convertible_to<std::unique_ptr<Subclass>>;
-}
+requires is_specialization<typename It::value_type, std::shared_ptr>
+      || is_specialization<typename It::value_type, std::unique_ptr>
 auto extract_subclass(const It &it) {
     return it | std::views::transform([](const auto &e) {
                return dynamic_cast<const Subclass *>(e.get());
            })
-           | std::views::filter([](const auto &e) { return e; });
+           | std::views::filter([](const auto *e) { return e; });
 }
 
 template <class Subclass, std::ranges::range It>
-requires requires(It it) {
-    { it[0] } -> std::assignable_from<Subclass *>;
-}
+requires std::derived_from<Subclass,
+                           std::remove_pointer_t<typename It::value_type>>
 auto extract_subclass(const It &it) {
-    return it | std::views::transform([](const auto *e) {
+    return it | std::views::transform([](const auto &e) {
                return dynamic_cast<const Subclass *>(e);
            })
            | std::views::filter([](const auto *e) { return e; });

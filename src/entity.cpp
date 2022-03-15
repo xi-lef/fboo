8161692c0f44f8b99700e3261f4c4a2928416710
Felix Windsheimer <felix.windsheimer@fau.de>
2022-03-15 16:32:31 +0100

    entity.cpp: improve output format

--- src/entity.cpp
+++ src/entity.cpp
@@ -18,23 +18,22 @@ std::string Ingredient::to_string() const {
 
 std::string Recipe::to_string() const {
     std::ostringstream ss;
-    ss << "Recipe '" << name << "' (" << category << "; initially "
-       << (enabled ? "enabled" : "disabled") << ") takes " << energy
-       << " ticks; takes: " << ingredients << "; produces: " << products;
+    ss << "Recipe '" << name << "'; " << category << "; initially "
+       << (enabled ? "enabled" : "disabled") << "; needs " << energy
+       << " ticks; takes " << ingredients << "; produces " << products;
     return ss.str();
 }
 
 std::string Factory::to_string() const {
     std::ostringstream ss;
-    ss << "Factory '" << name << "' can craft " << crafting_categories
+    ss << "Factory '" << name << "'; can craft " << crafting_categories
        << " at speed " << crafting_speed;
     return ss.str();
 }
 
 std::string Technology::to_string() const {
     std::ostringstream ss;
-    ss << "Technology '" << name << "'"
-       << "; requires: " << prerequisites << "; consumes: " << ingredients
-       << "; unlocks: " << unlocked_recipes;
+    ss << "Technology '" << name << "'; requires " << prerequisites
+       << "; consumes " << ingredients << "; unlocks " << unlocked_recipes;
     return ss.str();
 }

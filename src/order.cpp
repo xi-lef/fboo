d0476537f98e4b2fb1e906d820b271ad9bbed0c3
Felix Windsheimer <felix.windsheimer@fau.de>
2022-03-20 17:41:20 +0100

    order.cpp: intermediate changes

--- src/order.cpp
+++ src/order.cpp
@@ -32,9 +32,26 @@ EventList order::compute(const ItemMap &all_items, const RecipeMap &all_recipes,
         fid_map.insert(&f); // TODO only works for player
     }
 
+    std::unordered_map<std::string, int> needed_items;
+    std::deque<Ingredient> stack(goal_items.begin(), goal_items.end());
+    while (!stack.empty()) {
+        Ingredient i = stack.front();
+        stack.pop_front();
+        needed_items[i.get_name()] += i.get_amount();
+        for (const Ingredient &x :
+             all_recipes.at(i.get_name()).get_ingredients()) {
+            stack.push_back(x);
+        }
+    }
+    // TODO set<string> available_items, fill with items that have no prereqs,
+    // then iteratively as we get more items, to build factories for needed
+    // categories
+
+    std::clog << "needed_items: " << needed_items << std::endl;
+
     long long tick = 0;
-    for (const Ingredient &i : goal_items) {
-        const Recipe &r = all_recipes.at(i.get_name());
+    for (const auto &[name, amount] : needed_items) {
+        const Recipe &r = all_recipes.at(name);
         std::string needed_category = r.get_category();
         auto f = std::ranges::find_if(fid_map, [&](const auto &v) {
             return v.second->get_crafting_categories().contains(needed_category);
@@ -44,7 +61,7 @@ EventList order::compute(const ItemMap &all_items, const RecipeMap &all_recipes,
         }
         std::clog << f->second << std::endl;
         order.push_back(std::make_shared<StartEvent>(tick, f->first, r));
-        tick += f->second->calc_ticks(r);
+        tick += f->second->calc_ticks(r) * amount;
         order.push_back(std::make_shared<StopEvent>(tick, f->first));
     }
     //game::State state(all_recipes);

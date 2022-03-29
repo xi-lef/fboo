e9572ec3cdc6294f2f024121a42ed9a068900057
Felix Windsheimer <felix.windsheimer@fau.de>
2022-03-29 15:29:02 +0200

    use CMAKE_EXPORT_COMPILE_COMMANDS instead of bear

--- build.sh
+++ build.sh
@@ -8,11 +8,6 @@ if [ ! -d $build_dir ] || [ "$#" -ne 0 ]; then
     cmake -S . -B $build_dir
 fi
 
-if which bear 2> /dev/null 1>&2 \
-    && { [ ! -r compile_commands.json ] || [ "$#" -ne 0 ]; } then
-    prefix='bear --'
-fi
-
-${prefix:-} cmake --build $build_dir -j "$(nproc)"
+cmake --build $build_dir -j "$(nproc)"
 
 ln -fs $build_dir/apps/fboo
